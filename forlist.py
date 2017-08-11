8865618bdcd056a2a13f4e20cfc7402af71b6e99
zxc <zxcser@163.com>
2017-08-11 11:53:31 +0800

    love

---
+++ forlist.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+_author__ = 'Administrator'
+
+a = [1,2,3,12,12,12,6,8,9]
+b = a[:]
+# print a
+# print b
+for i in a:
+	# if i%2 ==0 and i%3 ==0:
+	a.remove(i)
+		# a=b
+	print a
